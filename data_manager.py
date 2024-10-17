95b34d213f47ff20aba1bca37118d37f38851d5b
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2024-10-17 06:25:52 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,27 +1,6 @@
-# Updating the "data_manager.py" file to include the specific error details as comments.
-
-error_details = """# Error Details:
-# {
-#     "src_id": "31906",
-#     "sname": "data_manager.py",
-#     "spath": "/",
-#     "rule_Id": "56110008",
-#     "rule_name": "Code line should not end with space character",
-#     "line_num": 45,
-#     "priority_code": "1",
-#     "priority_name": "Critical"
-# }
-"""
-
 # Updated content for the Python file
-updated_file_content = error_details + """# data_manager.py
+
+# data_manager.py
 
 def example_function():
-    print("This line ends with a space character!")  # This line has an extra space at the end. 
-"""
-
-# Write the updated content to the Python file
-with open(python_file_path, 'w') as file:
-    file.write(updated_file_content)
-
-python_file_path
+    print("This line ends with a space character!")  # This line has an extra space at the end.
