0ffa96c68d2ecbfce1b9981d07d20575922a2e83
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2024-12-27 18:13:30 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,5 @@
 def example_function():
     print("This line ends with a space character!")  # This line has an extra space at the end.
 def example_function():
-    print("This line is too long and should be split for better readability.")
-    print("When code is concise, it's easier to read and maintain.")
+    print("This line ends with a space character!" +
+          "\nThis line continues to demonstrate the need for shorter lines.")
