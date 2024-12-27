8258bb27175d41f19ec3acf2b6f85cb62eefd7ca
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2024-12-27 18:12:28 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,14 +1,2 @@
-# data_manager.py
-
-def example_function():
-    print("This line ends with a space character!")  # This line has an extra space at the end.
-
-import os
-
-# Use a configurable path for the Python file
-python_file_path = os.path.join(os.getcwd(), 'data_manager.py')
-
-# data_manager.py
-
 def example_function():
     print("This line ends with a space character!")  # This line has an extra space at the end.
