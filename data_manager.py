044ec110a907a8a700eb0084dcfd290e90d0aefd
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2024-12-27 17:17:12 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,8 +2,7 @@
 
 def example_function():
     print("This line ends with a space character!")  # This line has an extra space at the end.
-# data_manager.py
-
-def example_function():
-    print("This line ends with a space character!" + 
-          " This is a continuation of the previous line.")
+# Assuming the line exceeding length refers to a print line:
+# Line Split Example:
+    print("This is an example of a very long line that might exceed the recommended length for better readability, so we will wrap it here")
+    # Continue the next part of the logic here if necessary.
