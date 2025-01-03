19450d6d1d7c8805a61ad07f337c8542d9f87889
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-03 22:29:07 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,23 +1,9 @@
-# Updating the "data_manager.py" file to include the specific error details as comments.
-
-error_details = """# Error Details:
-# {
-#     "src_id": "31906",
-#     "sname": "data_manager.py",
-#     "spath": "/",
-#     "rule_Id": "56110008",
-#     "rule_name": "Code line should not end with space character",
-#     "line_num": 45,
-#     "priority_code": "1",
-#     "priority_name": "Critical"
-# }
-"""
-
 # Updated content for the Python file
+
 updated_file_content = error_details + """# data_manager.py
 
 def example_function():
-    print("This line ends with a space character!")  # This line has an extra space at the end. 
+    print("This line does not end with a space character!")  # This line is now clean of trailing whitespace.
 """
 
 # Write the updated content to the Python file
