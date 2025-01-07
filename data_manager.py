45cf8e86b3d5b0c1052e5c9b9847911b6044cd36
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-08 01:33:39 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,11 @@
 # Updated content for the Python file
-
 updated_file_content = error_details + """# data_manager.py
 
 def example_function():
-    print("This line ends with no trailing whitespace!")  # This line has been corrected to remove any extra space at the end.
+    print("This line ends with a space character!")  # This line has an extra space at the end.
 """
+
+# Remove the extra space after the exclamation mark in the print statement.
+
+def example_function():
+    print("This line ends with a space character!")
