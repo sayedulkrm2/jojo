5ebda0b006ce2e1348ae38b7dae7108b5ee86869
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-08 01:34:31 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,5 @@
 def example_function():
     print("This line ends with a space character!")  # This line has an extra space at the end.
-# Example function (with too many parameters removed or refactored)
-
-def improved_function(param1, param2):
-    # Instead of having many parameters, consider using a dictionary or a class for grouped parameters.
-    print(param1)
-    print(param2)
+def example_function():
+    """This function demonstrates a print statement with an intentional trailing space."""
+    print("This line ends with a space character!")  # This line has an extra space at the end.
