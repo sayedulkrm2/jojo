bdaf19f9963bac84aadbbf8c6e3fff048fcc1c9d
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-16 03:03:28 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,7 +2,9 @@
 
 error_details = """# Error Details:
 # {
-#     "src_id": "31906",
+"""This function prints a message indicating that it has an extra space at the end."""
+def example_function():
+    print("This line ends with a space character!")  # This line has an extra space at the end.
 #     "sname": "data_manager.py",
 #     "spath": "/",
 #     "rule_Id": "56110008",
