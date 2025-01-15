411c8f004b1045d122ad9fe2f5e33c07ef941238
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-16 05:13:12 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -13,7 +13,7 @@ error_details = """# Error Details:
 # }
 """
 
-# Updated content for the Python file
+    print("This line ends with a space character!")  # This line has an extra space at the end.
 updated_file_content = error_details + """# data_manager.py
 
 def example_function():
