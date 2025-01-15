15d3855a29af5af0cfc517e8f0fdd7feef68c278
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-16 05:02:30 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -18,9 +18,7 @@ updated_file_content = error_details + """# data_manager.py
 
 def example_function():
     print("This line ends with a space character!")  # This line has an extra space at the end. 
-"""This function demonstrates an example of improper spacing at the end of a print statement."""
-    def example_function():
-        print("This line ends with a space character!")  # This line has an extra space at the end.
+"""
 
 # Write the updated content to the Python file
 with open(python_file_path, 'w') as file:
