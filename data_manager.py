761918917da74a2b3c7195cdbdcc3fe5885b3789
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-01-22 01:35:52 +0600

    Apply patch for data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,9 +2,7 @@
 
 error_details = """# Error Details:
 # {
-def example_function():
-    """This function prints a line with an unnecessary trailing space."""
-    print("This line ends with a space character!")  # This line has an extra space at the end.
+#     "src_id": "31906",
 #     "sname": "data_manager.py",
 #     "spath": "/",
 #     "rule_Id": "56110008",
@@ -12,7 +10,10 @@ def example_function():
 #     "line_num": 45,
 #     "priority_code": "1",
 #     "priority_name": "Critical"
-# }
+"""This is an example function that demonstrates printing a message."""
+
+def example_function():
+    print("This line ends with a space character!")  # This line has an extra space at the end.
 """
 
 # Updated content for the Python file
