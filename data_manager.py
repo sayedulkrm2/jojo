d3400c9ad15a9dd09459e09a97f1a6fea0402be0
sayedulkrm2 <101005230+sayedulkrm2@users.noreply.github.com>
2025-03-26 10:54:25 +0600

    Fix vulnerability in data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -13,7 +13,8 @@ error_details = """# Error Details:
 # }
 """
 
-# Updated content for the Python file
+def example_function():
+    print("This line ends with a space character!")  # This line has an extra space at the end.
 updated_file_content = error_details + """# data_manager.py
 
 def example_function():
